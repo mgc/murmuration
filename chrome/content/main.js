76260a7a6c3a51a87e68814b4a56c64d93f640da
Stephen Lau <steve@grommit.com>
2009-04-14 14:56:53 -0700

    Reformat main.js

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -46,38 +46,24 @@ Murmuration.addComment = function(event) {
  * UI controller that is loaded into the main player window
  */
 Murmuration.Controller = {
-
-  /**
-   * Called when the window finishes loading
-   */
-  onLoad: function() {
-
-    // initialization code
-    this._initialized = true;
-    this._strings = document.getElementById("murmuration-strings");
+	onLoad: function() {
+		// initialization code
+		this._initialized = true;
+		this._strings = document.getElementById("murmuration-strings");
     
-    // Perform extra actions the first time the extension is run
-    if (Application.prefs.get("extensions.murmuration.firstrun").value) {
-      Application.prefs.setValue("extensions.murmuration.firstrun", false);
-      this._firstRunSetup();
-    }
-
-    // XXX TODO MOVE
-    //Components.utils.import('resource://murmuration/main.jsm');
-  },
+		// Perform extra actions the first time the extension is run
+		if (Application.prefs.get("extensions.murmuration.firstrun").value) {
+		  Application.prefs.setValue("extensions.murmuration.firstrun", false);
+		  this._firstRunSetup();
+		}
+	},
   
-  /**
-   * Called when the window is about to close
-   */
-  onUnLoad: function() {
-    this._initialized = false;
-  },
+	onUnLoad: function() {
+		this._initialized = false;
+	},
   
-  /**
-   * Perform extra setup the first time the extension is run
-   */
-  _firstRunSetup : function() {
-  }
+	_firstRunSetup : function() {
+	}
 };
 
 window.addEventListener("load", function(e) { Murmuration.Controller.onLoad(e); }, false);
